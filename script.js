7aceedc5253f3aba6fdf18d71e349d43f8cdbefb
EBon244 <132640232+EBon244@users.noreply.github.com>
2025-04-23 03:15:46 -0500

    Added a button that changes size of board and dynamically changes to fit board

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 const board = document.querySelector(".board");
+const sizeBtn = document.querySelector(".size");
+let boxSize = 660 / 16;
 
 
-for (i = 0; i < 16 * 16; i++) {
+for (let i = 0; i < 16 * 16; i++) {
   const box = document.createElement("div");
   box.classList.add('box');
+  box.style.width = `${boxSize}px`;
+  box.style.height = `${boxSize}px`;
 
   board.appendChild(box);
 
   box.addEventListener('mouseover', () => {
     box.style.backgroundColor = "black";
   })
+}
+
+
+// Create a new board
+sizeBtn.addEventListener("click", () => {
+  const input = prompt('Choose board size: 1-100');
+  const num = Number(input);
+
+  if (!Number.isInteger(num) || num < 1 || num > 100) {
+    alert('Error: Value must be an Integer between 1-100!');
+    return;
+  }
+
+  boardSize(num);
+});
+
+
+function boardSize(num) {
+  board.textContent = "";
+
+  for (i = 0; i < num * num; i++) {
+    const box = document.createElement("div");
+    box.classList.add('box');
+    boxSize = 660 / num;
+
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+
+    board.appendChild(box);
+
+    box.addEventListener('mouseover', () => {
+      box.style.backgroundColor = "black";
+    })
+  }
 }
