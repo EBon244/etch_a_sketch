b46098f4ad229ecd8d67ad455dcb11b861df2fce
EBon244 <132640232+EBon244@users.noreply.github.com>
2025-04-24 02:53:27 -0500

    Updated code with better named variables for ease of clarity

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
 const board = document.querySelector(".board");
 const sizeBtn = document.querySelector(".size");
-let boxSize = 660 / 16;
+const BOARD_SIZE = 660;
+let boxSize = BOARD_SIZE / 16;
 
-
-for (let i = 0; i < 16 * 16; i++) {
-  const box = document.createElement("div");
-  box.classList.add('box');
-  box.style.width = `${boxSize}px`;
-  box.style.height = `${boxSize}px`;
-
-  board.appendChild(box);
-
-  box.addEventListener('mouseover', () => {
-    box.style.backgroundColor = "black";
-  })
-}
-
-
-// Create a new board
-sizeBtn.addEventListener("click", () => {
-  const input = prompt('Choose board size: 1-100');
-  const num = Number(input);
-
-  if (!Number.isInteger(num) || num < 1 || num > 100) {
-    alert('Error: Value must be an Integer between 1-100!');
-    return;
-  }
-
-  boardSize(num);
-});
-
-
-function boardSize(num) {
+// Creates a Board
+function changeGridSize(gridSize) {
   board.textContent = "";
 
-  for (i = 0; i < num * num; i++) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const box = document.createElement("div");
     box.classList.add('box');
-    boxSize = 660 / num;
+    boxSize = BOARD_SIZE / gridSize;
 
     box.style.width = `${boxSize}px`;
     box.style.height = `${boxSize}px`;
@@ -49,3 +22,23 @@ function boardSize(num) {
     })
   }
 }
+
+// Prompt user for grid size
+sizeBtn.addEventListener("click", () => {
+  const input = prompt('Choose board size: 1-100');
+  const gridSize = Number(input);
+
+  if (!Number.isInteger(gridSize) || gridSize < 1 || gridSize > 100) {
+    alert('Error: Value must be an Integer between 1-100!');
+    return;
+  }
+
+  changeGridSize(gridSize);
+});
+
+//Create default grid 16 * 16
+changeGridSize(16)
+
+//TODO create a random rainbow trail
+//Opacity increase of 10%
+//Let user choose specific color
