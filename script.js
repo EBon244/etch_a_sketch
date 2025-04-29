3392ac8214c87a7f2feffd22b7c2557202c158e9
EBon244 <132640232+EBon244@users.noreply.github.com>
2025-04-29 03:04:05 -0500

    Added Shading and Pen Color option buttons for more variety

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const board = document.querySelector(".board");
 const sizeBtn = document.querySelector(".sizeBtn");
 const rainbowBtn = document.querySelector(".rainbowBtn");
+const shadeBtn = document.querySelector(".shadeBtn")
+const penColor = document.querySelector(".penColor")
 const BOARD_SIZE = 660;
 let boxSize = BOARD_SIZE / 16;
 let isRainbow = false;
+let isShade = false;
 
 // Creates a Board
 function changeGridSize(gridSize) {
@@ -22,8 +25,16 @@ function changeGridSize(gridSize) {
     box.addEventListener('mouseover', () => {
       if (isRainbow === true) {
         box.style.backgroundColor = `rgb(${random()},${random()},${random()})`;
+      } else if (isShade === true) {
+        let currentOpacity = box.dataset.opacity ? parseFloat(box.dataset.opacity) : 0;
+        if (currentOpacity < 1) {
+          currentOpacity += 0.1;
+          currentOpacity = Math.min(currentOpacity, 1); // cap at 1
+          box.dataset.opacity = currentOpacity;
+        }
+        box.style.backgroundColor = `rgba(165, 165, 165, ${currentOpacity})`;
       } else {
-        box.style.backgroundColor = "black";
+        box.style.backgroundColor = penColor.value;
       }
     })
   }
@@ -42,14 +53,26 @@ sizeBtn.addEventListener("click", () => {
   changeGridSize(gridSize);
 });
 
-//Switch rainbow on and off
+//Switch rainbow on/off
 rainbowBtn.addEventListener("click", () => {
   if (isRainbow === false) {
     isRainbow = true;
+    isShade = false;
   } else {
     isRainbow = false;
   }
-})
+});
+
+// Switch Shade on/off
+shadeBtn.addEventListener("click", () => {
+  if (isShade === false) {
+    isShade = true;
+    isRainbow = false;
+  } else {
+    isShade = false;
+  }
+});
+
 
 //Create default grid 16 * 16
 changeGridSize(16)
@@ -59,6 +82,7 @@ changeGridSize(16)
 function random() {
   return Math.floor(Math.random() * 255) + 1;
 }
-//TODO create a random rainbow trail
-//Opacity increase of 10%
-//Let user choose specific color
+
+
+// Clear button to reset board retain size
+// Erase individually erase board piece by piece
