26e74d1259bef2f76bf7e3d07e3b593a2a6aeb39
EBon244 <132640232+EBon244@users.noreply.github.com>
2025-04-30 02:46:07 -0500

    Reorganized code for better readability

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Etch_A_Sketch
 Project objective to make an Etch n Sketch using specifically flex box for a little challenge. At a later date I will change it to grid.
+
+Completed project that creates a browser based Etch-a-Sketch which users are able to draw on.
+Features: include an option for grid size, pen color variety, pen shading tool, erase function, a reset tool, and a randomized rainbow function;
+
+- HTML5
+- CSS3 (Flexbox layout)
+- JavaScript (DOM manipulation, event listeners)
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Dom Elements
 const board = document.querySelector(".board");
 const sizeBtn = document.querySelector(".sizeBtn");
 const rainbowBtn = document.querySelector(".rainbowBtn");
@@ -5,19 +6,23 @@ const shadeBtn = document.querySelector(".shadeBtn");
 const penColor = document.querySelector(".penColor");
 const resetBtn = document.querySelector(".resetBtn");
 const eraseBtn = document.querySelector(".eraseBtn");
+
+// Variables
 const BOARD_SIZE = 660;
 let boxSize = BOARD_SIZE / 16;
 let isRainbow = false;
 let isShade = false;
 let isErase = false;
 
-// Creates a Board
+// Functions
+
+// Create a Board
 function changeGridSize(gridSize) {
   board.textContent = "";
 
   for (let i = 0; i < gridSize * gridSize; i++) {
     const box = document.createElement("div");
-    box.classList.add('box');
+    box.classList.add("box");
     boxSize = BOARD_SIZE / gridSize;
 
     box.style.width = `${boxSize}px`;
@@ -25,7 +30,7 @@ function changeGridSize(gridSize) {
 
     board.appendChild(box);
 
-    box.addEventListener('mouseover', () => {
+    box.addEventListener("mouseover", () => {
       if (isRainbow === true) {
         box.style.backgroundColor = `rgb(${random()},${random()},${random()})`;
       } else if (isShade === true) {
@@ -39,24 +44,31 @@ function changeGridSize(gridSize) {
       } else {
         box.style.backgroundColor = penColor.value;
       }
-    })
+    });
   }
-};
+}
+
+// Generate a random number for RGB
+function random() {
+  return Math.floor(Math.random() * 255) + 1;
+}
+
+// Event Listeners
 
 // Prompt user for grid size
 sizeBtn.addEventListener("click", () => {
-  const input = prompt('Choose board size: 1-100');
+  const input = prompt("Choose board size: 1-100");
   const gridSize = Number(input);
 
   if (!Number.isInteger(gridSize) || gridSize < 1 || gridSize > 100) {
-    alert('Error: Value must be an Integer between 1-100!');
+    alert("Error: Value must be an Integer between 1-100!");
     return;
   }
 
   changeGridSize(gridSize);
 });
 
-//Switch rainbow on/off
+// Toggle rainbow mode 
 rainbowBtn.addEventListener("click", () => {
   if (isRainbow === false) {
     isRainbow = true;
@@ -66,7 +78,7 @@ rainbowBtn.addEventListener("click", () => {
   }
 });
 
-// Switch Shade on/off
+// Toggle shading mode 
 shadeBtn.addEventListener("click", () => {
   if (isShade === false) {
     isShade = true;
@@ -76,7 +88,7 @@ shadeBtn.addEventListener("click", () => {
   }
 });
 
-
+// Toggle eraser
 let previous = penColor.value;
 eraseBtn.addEventListener("click", () => {
   if (isErase === false) {
@@ -88,19 +100,12 @@ eraseBtn.addEventListener("click", () => {
   }
 });
 
+// Reset grid to default size
 resetBtn.addEventListener("click", () => {
   changeGridSize(16);
 });
 
+// Reset boolean
 
-//Create default grid 16 * 16
+// Initialize default grid
 changeGridSize(16);
-
-
-
-function random() {
-  return Math.floor(Math.random() * 255) + 1;
-};
-
-
-// Clear button to reset board retain size
